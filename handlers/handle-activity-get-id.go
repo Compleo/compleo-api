e343dbeeb3fe94b8261f5d0788d868a1bf7d0c86
Tommaso Brandinelli <48562227+MayonaiseMan@users.noreply.github.com>
2021-04-19 20:41:33 +0200

    [API]->Get Id

---
+++ handlers/handle-activity-get-id.go
@@ -0,0 +1,59 @@
+package handlers
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/Compelo/compleo-api/types"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func ActivityGetFromIDHanle(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Got a /activity/getbyid handle from ", r.RemoteAddr)
+	w.Header().Set("Content-Type", "application/json")
+
+	db, sqlError := sql.Open("mysql", sqlVal)
+	if sqlError != nil {
+		panic(sqlError.Error())
+	}
+
+	switch r.Method {
+	case "GET":
+		keys, err := r.URL.Query()["id"]
+		if !err || len(keys[0]) < 1 {
+			w.Write([]byte(`{"message": "error"}`))
+			return
+		}
+		id := keys[0]
+
+		w.WriteHeader(http.StatusOK)
+
+		//Eseguo la query
+		queyRes, queyErr := db.Query("SELECT * FROM `lavori` WHERE `ID`='" + id + "'")
+		if queyErr != nil {
+			fmt.Println(queyErr)
+			w.Write([]byte(`{"message": "error"}`))
+			return
+		}
+
+		//Creo l'array da ritornare
+		var array types.Activity
+		for queyRes.Next() {
+			var g types.Activity
+			queyRes.Scan(&g.ID, &g.IDUtente, &g.Tipo, &g.Titolo, &g.Testo)
+
+			array = g
+		}
+
+		//Create JSON
+		j, _ := json.Marshal(array)
+		w.Write([]byte(j))
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(`NOT SUPPORTED`))
+	}
+
+	db.Close()
+}
--- server.go
+++ server.go
@@ -38,6 +38,7 @@ func initHandlers() {
 	go http.HandleFunc("/activity/lid", handlers.ActivityListPERIDHandle)
 	go http.HandleFunc("/activity/listqual", handlers.ActivityListPerQualificheHandle)
 	go http.HandleFunc("/activity/listall", handlers.ActivityListAllHanle)
+	go http.HandleFunc("/activity/getbyid", handlers.ActivityGetFromIDHanle)
 
 	//Recensioni
 	go http.HandleFunc("/recensione", handlers.RecensioneHandler)
