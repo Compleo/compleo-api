35f5673af4726844d8b3a67aa15fb761f6b0796a
Leonardo Baldazzi <public.thesquirlyz97@gmail.com>
2021-05-07 21:18:04 +0200

    Completati handlers

--- handlers/handle-prenotazione.go
+++ handlers/handle-prenotazione.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strconv"
@@ -22,6 +23,36 @@ func PrenotazioneHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
 		//Prendi le prenotazioni di un utente
+		keys, err := r.URL.Query()["idUtente"]
+		if !err || len(keys[0]) < 1 {
+			w.Write([]byte(`{"message": "error"}`))
+			fmt.Println("Errore")
+			return
+		}
+		id := keys[0]
+
+		w.WriteHeader(http.StatusOK)
+
+		//Eseguo la query
+		queyRes, queyErr := db.Query("SELECT * FROM `prenotazioni` WHERE `IDRichiedente`='" + id + "'")
+		if queyErr != nil {
+			fmt.Println(queyErr)
+			w.Write([]byte(`{"message": "error"}`))
+			return
+		}
+
+		//Creo l'array da ritornare
+		var array []types.Prenotazione
+		for queyRes.Next() {
+			var g types.Prenotazione
+			queyRes.Scan(&g.ID, &g.IDLavoro, &g.IDRichiedente, &g.Stato, &g.Scelta)
+
+			array = append(array, g)
+		}
+
+		//Create JSON
+		j, _ := json.Marshal(array)
+		w.Write([]byte(j))
 	case "PUT":
 		//Inserisci una nuova prenotazione
 		err := r.ParseForm()
